24331bf253cd38ccc8890dff30169ba9137a6033
yang-mf <2964988165@qq.com>
2020-01-06 02:11:37 +0000

    i

---
+++ app/Http/Controllers/crypt/CryptController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\crypt;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CryptController extends Controller
+{
+
+    /** 加密 */
+    public function encrypt()
+    {
+        echo 11;
+    }
+
+    /** 解密 */
+    public function decrypt()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pay','AlipayController@pay');
+Route::get('/encrypt','crypt\CryptController@pay');
+Route::get('/decrypt','crypt\CryptController@pay');
 
-
-Route::get('/pay/notify','Pay\AlipayController@notify');
-Route::get('/pay/return','Pay\AlipayController@return');
-
-Route::post('/test','LoginController@test');        //注册方法
-Route::post('/login','LoginController@login');        //登录方法
-Route::post('/userList','LoginController@userList')->middleware('fangshua');        //登录方法
-
-
-
-
-Route::get('/set','MimaController@set');
-Route::get('/get','MimaController@get');
